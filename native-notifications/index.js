3ee21aea208c598f721e6adcdf3de85e72ac9f50
Bommezijn <nathan@mandelo.nl>
2022-09-19 08:50:43 +0200

    chore: Add body text to notifications

--- native-notifications/index.js
+++ native-notifications/index.js
@@ -2,14 +2,17 @@ console.log("Hello World");
 const btn = document.querySelector("#show-notification");
 
 /* 
-  Event that triggers on button click, first check whether the browser allows notifications
+Event that triggers on button click, first check whether the browser allows notifications.
 */
 
 btn.addEventListener("click", () => {
   Notification.requestPermission().then((permission) => {
+    console.log(permission);
     if (permission === "granted") {
       console.log("permission granted");
-      new Notification("Permission is Granted.");
+      new Notification("Permission is Granted.", {
+        body:"You will now receive notifications"
+      });
     }
   });
 });
